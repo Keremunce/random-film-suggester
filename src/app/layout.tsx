c96c14ddcefb61e751cb877971617f779a50942b
Kerem Unce <keremunce@hotmail.com>
2025-08-04 13:37:35 +0300

    BaseButton component created

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,23 +2,29 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { MovieProvider } from "./context/MovieContext";
 
+import { Poppins, Raleway, Roboto } from "next/font/google";
+
+const poppins = Poppins({
+	weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+	subsets: ["latin"],
+	variable: "--font-poppins",
+});
+
 export const metadata: Metadata = {
-  title: "Random Film Suggester",
-  description: "Discover random movies with ease!",
+	title: "Random Film Suggester",
+	description: "Discover random movies with ease!",
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en" data-theme="dark">
-      <body className="bg-gray-900 text-white">
-        <MovieProvider>
-          {children}
-        </MovieProvider>
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en" data-theme="dark" className={`${poppins.variable}`}>
+			<body className="bg-gray-900 text-white">
+				<MovieProvider>{children}</MovieProvider>
+			</body>
+		</html>
+	);
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 "use client";
 import Navbar from "@/components/Navbar";
 import {BaseInput} from "@/components/BaseInput";
+import {BaseButton} from "@/components/BaseButton";
 
 export default function Home() {
 	return (
 		<main className="p-8">
 			<Navbar></Navbar>
 			<BaseInput variant="search" size="lg"  idFor="baseinput2" label="Search Movies or TV Shows" iconPath="/icons/search-normal.svg" placeholder="Quentin Tarantino Movies"></BaseInput>
+			<BaseButton value="button"></BaseButton>
 		</main>
 	);
 }
