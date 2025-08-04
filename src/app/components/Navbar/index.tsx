51440caac1a7097b25d4569f54b0c97ee92866f4
Kerem Unce <keremunce@hotmail.com>
2025-08-04 11:14:27 +0300

    components folder

---
+++ src/app/components/Navbar/index.tsx
@@ -0,0 +1,36 @@
+import Link from "next/link";
+import Image from "next/image";
+import styles from "./style.module.css";
+export default function Navbar() {
+	return (
+		<nav className={styles.navbar}>
+			<div className={styles.navbarContainer}>
+				<div className={styles.navbarLogo}>
+					<Image
+						src="/logo/Icon Only=True.svg"
+						alt="Logo"
+						width={50}
+						height={50}
+					/>
+				</div>
+				<ul className={styles.navbarLinks}>
+					<li className={styles.navbarLink}>
+						<Link href="/" className={styles.linkMovies}>
+							Movies
+						</Link>
+					</li>
+					<li className={styles.navbarLink}>
+						<Link href="/about" className={styles.linkShows}>
+							TV Shows
+						</Link>
+					</li>
+					<li className={styles.navbarLink}>
+						<Link href="/contact" className={styles.linkSuggest}>
+							Suggest Me <Image src="/icons/arrow-right.svg" alt="Arrow Right Icon" width={20} height={20} />
+						</Link>
+					</li>
+				</ul>
+			</div>
+		</nav>
+	);
+}
