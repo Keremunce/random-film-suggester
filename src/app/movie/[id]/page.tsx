7b1f6a4a26d8cdbfc55532ffc4e129fcdec19936
Kerem Unce <keremunce@hotmail.com>
2025-08-06 18:43:00 +0300

    movie detail page is complete

--- src/app/movie/[id]/page.tsx
+++ src/app/movie/[id]/page.tsx
@@ -1,6 +1,9 @@
 "use client";
 import { useEffect, useState } from "react";
 import { use } from "react";
+import Styles from "./style.module.css";
+import { RatingButton } from "@/app/components/RatingButton";
+import Navbar from "@/components/Navbar";
 
 type MovieDetailProps = {
 	params: {
@@ -30,20 +33,55 @@ export default function MovieDetail({ params }: MovieDetailProps) {
 	}
 
 	return (
-		<main className="movieDetailContainer">
-			<h1 className="movieTitle">{movie.title}</h1>
-			<img
-				src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-				alt={movie.title}
-				className="moviePoster"
-			/>
-			<p className="movieOverview">{movie.overview}</p>
-			<p className="movieReleaseDate">Release Date: {movie.release_date}</p>
-			<p className="movieVoteAverage">Rating: {movie.vote_average}</p>
-			<p className="movieVoteCount">Vote Count: {movie.vote_count}</p>
-			<p className="moviePopularity">Popularity: {movie.popularity}</p>
-			<p className="movieOriginalLanguage">Language: {movie.original_language}</p>
-			<p className="movieAdult">Adult Content: {movie.adult ? "Yes" : "No"}</p>
-		</main>
+		<div className="container">
+            <Navbar></Navbar>
+			<main className={Styles.movieDetailContainer}>
+				<div className={Styles.moviePosterContainer}>
+					<img
+						src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+						alt={movie.title}
+						className={Styles.moviePoster}
+					/>
+				</div>
+				<div className={Styles.movieContentContainer}>
+					<div>
+						<h1 className={Styles.movieTitle}>{movie.title}</h1>
+					</div>
+					<div>
+						<p className={Styles.movieOverview}>{movie.overview}</p>
+					</div>
+					<div className={Styles.ratingContainer}>
+						<RatingButton rating={movie.vote_average}></RatingButton>
+					</div>
+
+					<div className={Styles.subContentContainer}>
+						<p className={Styles.subTitle}>Release Date:</p>
+						<p className={Styles.movieReleaseDate}>{movie.release_date}</p>
+					</div>
+					<div className={Styles.subContentContainer}>
+						<p className={Styles.subTitle}>Rating:</p>
+						<p className={Styles.movieVoteAverage}>{movie.vote_average}</p>
+					</div>
+					<div className={Styles.subContentContainer}>
+						<p className={Styles.subTitle}>Vote Count:</p>
+						<p className={Styles.movieVoteCount}>{movie.vote_count}</p>
+					</div>
+					<div className={Styles.subContentContainer}>
+						<p className={Styles.subTitle}>Popularity:</p>
+						<p className={Styles.moviePopularity}>{movie.popularity}</p>
+					</div>
+					<div className={Styles.subContentContainer}>
+						<p className={Styles.subTitle}>Language:</p>
+						<p className={Styles.movieOriginalLanguage}>
+							{movie.original_language}
+						</p>
+					</div>
+					<div className={Styles.subContentContainer}>
+						<p className={Styles.subTitle}>Adult Content:</p>
+						<p className={Styles.movieAdult}>{movie.adult ? "Yes" : "No"}</p>
+					</div>
+				</div>
+			</main>
+		</div>
 	);
 }
